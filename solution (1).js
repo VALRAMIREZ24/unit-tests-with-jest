fec91e755b6ccfb1ef519ca867838dd0bf09ed6d
VALERIE <124011468+VALRAMIREZ24@users.noreply.github.com>
2023-08-19 20:19:47 -0500

    Update solution (1).js
    correction

--- solution (1).js
+++ solution (1).js
@@ -1,16 +1,15 @@
 function findLowestScoringStudent(students) {
   describe("findLowestScoringStudent", () => {
   it("should return the student with the lowest score", () => {
-    const mathStudents = [
-      { name: "Shane Carey", score: 9.5 },
-      { name: "Catarina Lima", score: 9.7 },
-      { name: "Rebecca Mills", score: 8.7 },
-    ];
-
-    const expected = { name: "Catarina Lima", score: 9.7 };
-    const actual = LowestScoringStudent(mathStudents);
-
-    expect(actual).toEqual(expected);
+    const Students = [
+    { name: "Leo Yeon-Joo", score: 8.9 },
+    { name: "Morgan Sutton", score: 7.4 },
+    { name: "Natalee Vargas", score: 9.2 },
+  ];
+  
+  const expected = { name: "Morgan Sutton", score: 7.4 };
+  const actual = findLowestScoringStudent(Students);
+  
+  expect(actual).toEqual(expected); 
   });
 });
-module.exports = findLowestScoringStudent;
